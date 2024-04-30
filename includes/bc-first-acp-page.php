0810184ce56976e02ca17b0bc7fa06a3082a1185
mnicolau <mnicol29@xtec.cat>
2024-04-30 22:29:14 +0200

    init

---
+++ includes/bc-first-acp-page.php
@@ -0,0 +1,66 @@
+<div class="wrap">
+            <h1>Hola!</h1>
+            <p>Aquesta és la primera pàgina del plugin</p>
+
+        </div>
+
+<?php
+
+function data_table($db_data)
+{
+    if (!is_array($db_data) || empty($db_data))
+        return false;
+
+    // Get the table header cells by formatting first row's keys
+    $header_vals = array();
+    $keys = array_keys($db_data[0]);
+    foreach ($keys as $row_key) {
+        $header_vals[] = ucwords(str_replace('_', ' ', $row_key)); // capitalise and convert underscores to spaces
+    }
+    $header = "<thead><tr><th>" . join('</th><th>', $header_vals) . "</th></tr></thead>";
+
+    // Make the data rows
+    $rows = array();
+    foreach ($db_data as $row) {
+        $row_vals = array();
+        foreach ($row as $key => $value) {
+
+            // format any date values properly with WP date format
+            if (strpos($key, 'date') !== false || strpos($key, 'modified') !== false) {
+                $date_format = get_option('date_format');
+                $value = mysql2date($date_format, $value);
+            }
+            $row_vals[] = $value;
+        }
+        $rows[] = "<tr><td>" . join('</td><td>', $row_vals) . "</td></tr>";
+    }
+
+    // Put the table together and output
+    echo '<table class="wp-list-table widefat fixed posts">' . $header . '<tbody>' . join($rows) . '</tbody></table>';
+
+    return true;
+}
+
+/* Example usage with a menu page */
+function database_table_example_page()
+{
+
+    // example query: ALUMNES DEL CENTRE
+    global $wpdb;
+    $query = "SELECT $wpdb->alumnes.*
+        FROM $wpdb->alumnes";
+    $result = $wpdb->get_results($query, ARRAY_N);
+
+    ?>
+        <div class="wrap">
+            <h1>Hola!</h1>
+            <p>Aquesta  és la primera pàgina del plugin</p>
+
+            <?php data_table($result); ?>
+        </div>
+        
+    <?php
+}
+
+
+?>
---
+++ includes/bc-functions.php
@@ -0,0 +1,20 @@
+<?php
+/*
+ * Afegir un nou menu al Panell de Control d'Admin
+ */
+// Hook d'acció 'admin_menu', executa la funció  'bc_Add_My_Admin_Link()'
+add_action( 'admin_menu', 'bc_Add_My_Admin_Link' );
+// Afegir un enllaç de menu de nivell superior al Panlel de Control
+function bc_Add_My_Admin_Link() {
+      add_menu_page(
+        'Pàgina Bosc de la Coma', // Title of the page
+        'Bosc de la Coma Plugin', // Text to show on the menu link
+        'manage_options', // Capability requirement to see the link
+        BC_DIR.'includes/bc-first-acp-page.php' // The 'slug' - file to display when clicking the link
+    );
+    add_submenu_page(BC_DIR.'includes/bc-first-acp-page.php', 
+                    'Alumnes', 'Alumnes', 'manage_options',
+                     BC_DIR.'includes/bc-alumnes.php');
+    remove_submenu_page( BC_DIR.'includes/bc-alumnes.php',BC_DIR.'includes/bc-alumnes.php' );
+
+}
